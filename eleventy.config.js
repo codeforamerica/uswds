e535df55887ac96e2027da8d8c72a85068ff045b
Devon Hirth <dhirth@codeforamerica.org>
2023-04-05 13:47:59 -0400

    0.0.0-8

--- eleventy.config.js
+++ eleventy.config.js
@@ -1,6 +1,8 @@
-
 const package = require('./package.json');
 const eleventyPluginTwig = require('@factorial/eleventy-plugin-twig');
+const twig = require('twig');
+const fs = require('fs');
+const beautify = require('js-beautify').html;
 
 module.exports = function(config) {
   config.addGlobalData('package', package);
@@ -10,6 +12,36 @@ module.exports = function(config) {
       layouts: 'views/_includes/',
       input: 'views',
       output: 'dist'
+    },
+    twig: {
+      shortcodes: [
+        {
+          symbol: 'beautify',
+          callback: (eleventyConfig, userOptions, inputPath, data) => {
+            twig.cache(false);
+
+            const template = twig.twig({
+              data: data,
+              data: fs.readFileSync(inputPath, 'utf-8'),
+              path: inputPath
+            });
+
+            // console.dir(template);
+
+            rendered = template.render(data);
+
+            return beautify(`${rendered}<!-- template source: ${inputPath} -->`, {
+              indent_size: 2,
+              indent_char: ' ',
+              preserve_newlines: false,
+              indent_inner_html: false,
+              inline: [],
+              wrap_line_length: 0,
+              indent_inner_html: false
+            });
+          }
+        }
+      ]
     }
   });
 
